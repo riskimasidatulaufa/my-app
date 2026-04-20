650cd71637123979206e0623d2d2f06878380d8e
unknown <riskimasidatul0109@gmail.com>
2026-04-20 12:38:05 +0800

    first commit

---
+++ app/learn/page.tsx
@@ -0,0 +1,16 @@
+
+export default function learnpage() {
+    return (
+   <div>
+    <div>
+        <img src="/palette.svg" alt="design"/>
+        <p>design</p>
+    </div>
+    <div>
+        <p>figma</p>
+        <p>8 hours ago</p>
+    </div>
+   </div>
+
+);
+}
