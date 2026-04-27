37259c5338164d04e836270bd4f62e362b5d704b
unknown <riskimasidatul0109@gmail.com>
2026-04-27 12:35:13 +0800

    update css

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,16 +1,42 @@
 
 export default function learnpage() {
     return (
-   <div>
-    <div>
+        <div className= "h-screen bg-gray-100 flex gap-4">
+   <div className="bg-purple-300 p-8 h-72 w-96 rounded-4xl flex flex-col justify-between text-gray-900">
+    <div className="flex gap-4">
         <img src="/palette.svg" alt="design"/>
         <p>design</p>
     </div>
     <div>
-        <p>figma</p>
-        <p>8 hours ago</p>
+        <p className="text-4xl font bold">figma</p>
+        <p className="font-bold">8 hours ago</p>
     </div>
    </div>
+
+   <div className="bg-pink-300 p-8 h-72 w-96 rounded-4xl flex flex-col justify-between text-gray-900">
+    <div className="flex gap-4">
+        <img src="/palette.svg" alt="design"/>
+        <p>design</p>
+    </div>
+    <div>
+        <p className="text-4xl font bold">figma</p>
+        <p className="font-bold">8 hours ago</p>
+    </div>
+   </div>
+
+
+   <div className="bg-green-300 p-8 h-72 w-96 rounded-4xl flex flex-col justify-between text-gray-900">
+    <div className="flex gap-4">
+        <img src="/palette.svg" alt="design"/>
+        <p>design</p>
+    </div>
+    <div>
+        <p className="text-4xl font bold">figma</p>
+        <p className="font-bold">8 hours ago</p>
+    </div>
+   </div>
+   </div>
+
 
 );
 }
