f06b6d09b6c04613b0876aa39651f5a26e602c7a
unknown <riskimasidatul0109@gmail.com>
2026-04-29 15:06:17 +0800

    task-2

---
+++ app/task-2/page.tsx
@@ -0,0 +1,105 @@
+export default function learnpage() {
+    return (
+            <div className= "relative min-h-screen bg-gray-100 flex flex-col justify-center items-center gap-4">
+                <div className= "absolute top-12 left-12">
+                    <img src="/chevrons-up.svg" alt="chevrons-up" className="w-8 h-8" />
+                </div>
+                <div className= "absolute bottom-6 right-6 text-gray-400 text-xl">
+                    <p>Day 020/365</p>
+                </div>
+            <div className="flex gap-4">
+                
+
+            <div className="relative bg-blue-100 p-8 h-72 w-122 rounded-4xl flex flex-col justify-between text-gray-900">
+    <div className="flex gap-4">
+        <img src="/palette.svg" alt="design"/>
+        <p>Design</p>
+    </div>
+    
+    <div>
+        <p className="text-4xl font bold">Adobe Photoshop</p>
+        <p className="font-bold">in 3 days</p>
+    </div>
+    <div className="absolute bottom-5 right-5 flex -space-x-4 items-center">
+             <img src="https://randomuser.me/api/portraits/women/44.jpg" alt="User 1" className="w-10 h-10 rounded-full border-4 border-white object-cover ring-1 ring-black/5"/>
+            
+            <img src="https://randomuser.me/api/portraits/men/32.jpg" alt="User 2" className="w-10 h-10 rounded-full border-4 border-white object-cover ring-1 ring-black/5"/>
+
+            <img src="https://randomuser.me/api/portraits/men/30.jpg" alt="User 3" className="w-10 h-10 rounded-full border-4 border-white object-cover ring-1 ring-black/5"/>
+           
+            <div className="w-10 h-10 rounded-full bg-white border-4 border-white flex items-center justify-center ring-1 ring-black/5">
+              <span className="text-xl font-bold text-blue-950">9+</span>
+            </div>
+          </div>
+   </div>
+
+
+
+<div className="relative bg-green-100 p-8 h-72 w-122 rounded-4xl flex flex-col justify-between text-gray-900">
+    <div className="absolute top-6 right-6">
+        <img src="/star.svg" alt="star" className="w-6 h-6" />
+    </div>
+    <div className="flex gap-4">
+        <img src="/terminal-2.svg" alt="design"/>
+        <p>AI</p>
+    </div>
+    <div>
+        <p className="text-4xl font bold">DALL·E 2, Midjourney, Stable Diffusion</p>
+        <p className="font-bold">in 5 days</p>
+    </div>
+    <div className="absolute bottom-5 right-5 flex -space-x-4 items-center">
+             <img src="https://randomuser.me/api/portraits/women/44.jpg" alt="User 1" className="w-10 h-10 rounded-full border-4 border-white object-cover ring-1 ring-black/5"/>
+            
+            <img src="https://randomuser.me/api/portraits/men/32.jpg" alt="User 2" className="w-10 h-10 rounded-full border-4 border-white object-cover ring-1 ring-black/5"/>
+
+            <img src="https://randomuser.me/api/portraits/men/30.jpg" alt="User 3" className="w-10 h-10 rounded-full border-4 border-white object-cover ring-1 ring-black/5"/>
+           
+            <div className="w-10 h-10 rounded-full bg-white border-4 border-white flex items-center justify-center ring-1 ring-black/5">
+              <span className="text-xl font-bold text-blue-950">9+</span>
+            </div>
+          </div>
+   </div>
+</div>
+
+
+   <div className="flex gap-4">
+   <div className="bg-purple-100 p-8 h-72 w-80 rounded-4xl flex flex-col justify-between text-gray-900">
+    <div className="flex gap-4">
+        <img src="/palette.svg" alt="design"/>
+        <p>Design</p>
+    </div>
+    <div>
+        <p className="text-4xl font bold">Figma</p>
+        <p className="font-bold">8 hours ago</p>
+    </div>
+   </div>
+
+   <div className="bg-orange-100 p-8 h-72 w-80 rounded-4xl flex flex-col justify-between text-gray-900">
+    <div className="flex gap-4">
+        <img src="/code.svg" alt="design"/>
+        <p>Codding</p>
+    </div>
+    <div>
+        <p className="text-4xl font bold">Pyton</p>
+        <p className="font-bold">2 days ago</p>
+    </div>
+   </div>
+
+
+   <div className="bg-red-100 p-8 h-72 w-80 rounded-4xl flex flex-col justify-between text-gray-900">
+    <div className="flex gap-4">
+        <img src="/palette.svg" alt="design"/>
+        <p>Design</p>
+    </div>
+    <div>
+        <p className="text-4xl font bold">Sketch</p>
+        <p className="font-bold">4 days ago</p>
+    </div>
+   </div>
+   </div>
+   </div>
+
+
+    
+);
+}
